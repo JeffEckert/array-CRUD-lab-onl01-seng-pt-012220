8f975bb92b282a6bf048bb0c3b0de8d498cc7956
Jeffery Eckert <bromodo@gmail.com>
2020-01-29 01:31:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  elements = ["wind", "water", "fire", "earth"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  elements = ["wind", "water", "fire", "earth"]
+  elements << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
