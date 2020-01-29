bb33e80c59d502f7cd9dbca4d43c16bf451bac18
Jeffery Eckert <bromodo@gmail.com>
2020-01-29 01:36:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   elements = ["wind", "water", "fire", "earth"]
-  elements.unshift("first")
+  elements.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
